c730d138edaac3036a32786981676c44808abbda
daengdaengLee <gunho1020@gmail.com>
2023-08-31 22:40:52 +0900

    feat: Item 엔티티를 Jpa Entity 로 선언
    - id 필드를 pk 로 선언
    - id 는 자동으로 증가하면서 생성되도록 설정

--- src/main/java/hello/until/item/entity/Item.java
+++ src/main/java/hello/until/item/entity/Item.java
@@ -1,5 +1,9 @@
 package hello.until.item.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,7 +11,10 @@ import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@Entity
 public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private Integer price;
