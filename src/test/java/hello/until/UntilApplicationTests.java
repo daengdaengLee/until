c28df64eb5ae1e828c124967bf4eaac0f0d0fa2f
daengdaengLee <gunho1020@gmail.com>
2023-08-31 12:38:13 +0900

    feat: 프로젝트 초기화
    - 아래 링크에서 정리한 내용대로 프로젝트 생성
    - https://github.com/daengdaengLee/until/issues/2#issuecomment-1699149609

---
+++ src/test/java/hello/until/UntilApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.until;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UntilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
