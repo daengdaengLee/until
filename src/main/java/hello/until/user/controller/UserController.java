bf3871e346c3bdf7a8587b11d6c6da883a2150f8
jinseong <jinseong205@gmail.com>
2023-08-31 16:34:59 +0900

    feat : User 생성 기능 구현
    유저 생성 dto 추가. validation 추가
    controller , service각 anotation을 이용하여 빈으로 등록 및 유저 생성 기능 구현
    jpaRepostiroy 상속 및 유저 생성 기능 구현
    resolves: #7

--- src/main/java/hello/until/user/controller/UserController.java
+++ src/main/java/hello/until/user/controller/UserController.java
@@ -1,4 +1,26 @@
 package hello.until.user.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import hello.until.user.dto.request.CreateUserRequest;
+import hello.until.user.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
 public class UserController {
+	
+	private final UserService userService;
+	
+	@PostMapping("/join")
+	public ResponseEntity<?> join(@RequestBody @Valid CreateUserRequest createUserRequest){
+		userService.createUser(createUserRequest.getEmail(), createUserRequest.getPassword());
+		return ResponseEntity.ok().build();
+	}
+	
+	
 }
--- src/main/java/hello/until/user/service/UserService.java
+++ src/main/java/hello/until/user/service/UserService.java
@@ -1,4 +1,38 @@
 package hello.until.user.service;
 
+import java.time.LocalDateTime;
+
+import org.springframework.stereotype.Service;
+
+import hello.until.user.entity.User;
+import hello.until.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+	
+	private final UserRepository userRepository;
+	
+	public void createUser(String email, String password) {
+		
+		LocalDateTime currentDateTime = LocalDateTime.now();
+		
+		validateUser(email);
+		
+		User user = new User();
+		user.setEmail(email);
+		user.setPassword(password);
+		user.setCreateAt(currentDateTime);
+		user.setUpdateAt(currentDateTime);
+		userRepository.save(user);
+		
+	}
+	
+	public void validateUser(String eamil) {
+		User findUser = userRepository.findByEmail(eamil);
+		if(findUser != null) {
+			throw new IllegalStateException("이미 가입된 회원 메일입니다.");
+		}
+	}
 }
