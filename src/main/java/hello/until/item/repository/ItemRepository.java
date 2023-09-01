8312db109878b9ebcf6ddc84102585cac8fd91fd
tableminpark <tablemin_park@daum.net>
2023-09-01 13:06:02 +0900

    feat : ItemRepository 인터페이스 Repository Bean 으로 선언

--- src/main/java/hello/until/item/repository/ItemRepository.java
+++ src/main/java/hello/until/item/repository/ItemRepository.java
@@ -2,6 +2,8 @@ package hello.until.item.repository;
 
 import hello.until.item.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
 }
