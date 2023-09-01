9105cec7b80cbfacb250b90116e67ea8fa40d2ef
Kunho Lee <gunho1020@gmail.com>
2023-09-02 02:15:47 +0900

    상품 단일 조회 서비스 만들기 (#19)
    * feat: ItemService#readItem 개발
    
    - Item 단건 조회 기능
    - id 로 조회
    - Repository 에서 이미 Optional 로 반환 -> 그대로 반환
    
    * test: ItemService#readItem 테스트 작성
    
    - 존재하는 Item 을 조회하는 경우
    
    * test: ItemService#readItem 테스트 작성
    
    - 존재하지 않는 Item 을 조회하는 경우

---
+++ src/main/java/hello/until/item/service/ItemService.java
@@ -0,0 +1,18 @@
+package hello.until.item.service;
+
+import hello.until.item.entity.Item;
+import hello.until.item.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class ItemService {
+    private final ItemRepository itemRepository;
+
+    public Optional<Item> readItem(Long id) {
+        return this.itemRepository.findById(id);
+    }
+}
