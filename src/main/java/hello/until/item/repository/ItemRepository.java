de9aaf7845812dd177177357e33980ed184bbd9f
tableminpark <tablemin_park@daum.net>
2023-09-01 14:12:18 +0900

    feat : ItemRepository 인터페이스 @Repository 삭제

--- src/main/java/hello/until/item/repository/ItemRepository.java
+++ src/main/java/hello/until/item/repository/ItemRepository.java
@@ -4,6 +4,5 @@ import hello.until.item.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
 }
