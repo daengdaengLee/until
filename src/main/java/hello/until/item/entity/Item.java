563d836abc14440a7a2463c9906b84766b9cbd98
daengdaengLee <gunho1020@gmail.com>
2023-09-02 12:07:25 +0900

    feat: Item 엔티티에 builder 추가

--- src/main/java/hello/until/item/entity/Item.java
+++ src/main/java/hello/until/item/entity/Item.java
@@ -1,8 +1,7 @@
 package hello.until.item.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -11,6 +10,9 @@ import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @EntityListeners(AuditingEntityListener.class)
 public class Item {
