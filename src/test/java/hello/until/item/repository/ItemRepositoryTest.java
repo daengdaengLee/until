9be9be03b13b8e1e2a623204e76409b2a0654033
daengdaengLee <gunho1020@gmail.com>
2023-09-02 12:49:24 +0900

    fix: DataJapTest 에 AutoConfigureTestDatabase 추가
    - USER 예약에 때문에 추가함
    - 나중에 datasource.url 을 실제 DB 로 바꿀 때 test profile 에서 h2 db url 설정 지금처럼 유지해야 할 듯
    - 예약어 문제 다른 방법으로 풀면 제거 가능

--- src/test/java/hello/until/item/repository/ItemRepositoryTest.java
+++ src/test/java/hello/until/item/repository/ItemRepositoryTest.java
@@ -5,6 +5,7 @@ import org.awaitility.Awaitility;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.time.Duration;
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class ItemRepositoryTest {
     @Autowired
     private ItemRepository itemRepository;
