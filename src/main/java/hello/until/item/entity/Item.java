c8ad4df2d8a32607c83d986327ddf1943b583c08
daengdaengLee <gunho1020@gmail.com>
2023-09-02 12:05:23 +0900

    feat: Item 엔티티에 생성일, 수정일 자동 검사 기능 추가
    - JPA 의 AuditingEntityListener 이벤트 리스너 추가
    - @CreatedAt 로 생성일 자동 추적
    - @LastModifiedDate 로 수정일 자동 추적
    - createdAt 은 수정 불가 column 으로 지정

--- src/main/java/hello/until/item/entity/Item.java
+++ src/main/java/hello/until/item/entity/Item.java
@@ -1,23 +1,27 @@
 package hello.until.item.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class Item {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private Integer price;
+    @Column(updatable = false)
+    @CreatedDate
     private LocalDateTime createdAt;
+    @LastModifiedDate
     private LocalDateTime updatedAt;
 }
