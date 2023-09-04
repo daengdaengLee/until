dfdea0d961958724379685cf017829c3661f0401
jinseong <jinseong205@gmail.com>
2023-09-04 15:55:19 +0900

    feat: 컨트롤러 오류처리 수정
    컨트롤러 오류 처리 수정하여 업로드

--- src/main/java/hello/until/user/controller/UserController.java
+++ src/main/java/hello/until/user/controller/UserController.java
@@ -1,5 +1,8 @@
 package hello.until.user.controller;
 
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,7 +15,6 @@ import hello.until.user.dto.request.CreateUserRequest;
 import hello.until.user.dto.response.GetUserResponse;
 import hello.until.user.entity.User;
 import hello.until.user.service.UserService;
-import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
@@ -20,20 +22,24 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @RequestMapping("/users")
 public class UserController {
-	
+
 	private final UserService userService;
 
 	@PostMapping("/join")
-	public ResponseEntity<?> join(@RequestBody @Valid CreateUserRequest createUserRequest){
+	public ResponseEntity<?> join(@RequestBody @Valid CreateUserRequest createUserRequest) {
 		userService.createUser(createUserRequest.getEmail(), createUserRequest.getPassword());
 		return ResponseEntity.ok().build();
 	}
-	
-	
-    @GetMapping("/{id}")
-    public ResponseEntity<?> getUserById(@PathVariable long id){
-        User user = userService.getUserById(id).orElseThrow(() -> new EntityNotFoundException("해당 유저를 찾을 수 없습니다."));
-        return ResponseEntity.ok(new GetUserResponse(user));
-    }
-	
+
+	@GetMapping("/{id}")
+	public ResponseEntity<?> getUserById(@PathVariable long id) {
+
+		Optional<User> user = userService.getUserById(id);
+
+		if (user.isPresent())
+			return ResponseEntity.ok(new GetUserResponse(user.get()));
+		else
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("해당 유저를 찾을 수 없습니다.");
+	}
+
 }
