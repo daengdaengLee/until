5e324e16862a7bdaead1d8291daca4015f222ae9
daengdaengLee <gunho1020@gmail.com>
2023-09-04 12:29:10 +0900

    feat: 상품 수정 기능 구현

--- src/main/java/hello/until/item/service/ItemService.java
+++ src/main/java/hello/until/item/service/ItemService.java
@@ -1,16 +1,19 @@
 package hello.until.item.service;
 
+import hello.until.exception.CustomException;
+import hello.until.exception.ExceptionCode;
 import hello.until.item.entity.Item;
 import hello.until.item.repository.ItemRepository;
+import lombok.Builder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
 import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
+@Transactional(readOnly = true)
 public class ItemService {
     private final ItemRepository itemRepository;
 
@@ -26,5 +29,19 @@ public class ItemService {
                 .build();
 
         return itemRepository.save(item);
+    }
+
+    @Transactional
+    public Item updateItem(UpdateItemInputDto inputDto) {
+        var item = this.itemRepository.findById(inputDto.id)
+                .orElseThrow(() -> new CustomException(ExceptionCode.NO_ITEM_TO_UPDATE));
+        item.setName(inputDto.name);
+        item.setPrice(inputDto.price);
+        this.itemRepository.save(item);
+        return item;
+    }
+
+    @Builder
+    public record UpdateItemInputDto(long id, String name, int price) {
     }
 }
