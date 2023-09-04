6bee0dd4f6366a00a9acb8551fc0742a8d775a70
dudwns9280 <chlqus9984@gmail.com>
2023-09-04 17:43:02 +0900

    test : repository test case 작성

--- src/test/java/hello/until/user/repository/UserRepositoryTest.java
+++ src/test/java/hello/until/user/repository/UserRepositoryTest.java
@@ -38,4 +38,28 @@ public class UserRepositoryTest {
         assertEquals(dbUser.getCreatedAt(), currentDateTime);
         assertEquals(dbUser.getUpdatedAt(), currentDateTime);
     }
+
+    @Test
+    @DisplayName("회원 정보 수정")
+    public void updateUser() {
+        // given
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        User user = new User();
+        user.setId(1L);
+        user.setEmail("test@test.com");
+        user.setPassword("12345678");
+        user.setCreatedAt(currentDateTime);
+        user.setUpdatedAt(currentDateTime);
+        User dbUser = userRepository.save(user);
+        LocalDateTime updateDateTime = LocalDateTime.now();
+        dbUser.updateUser("test2@test.com", null, updateDateTime);
+        // when
+        User savedUser = userRepository.save(dbUser);
+
+        // then
+        assertEquals(dbUser.getEmail(), savedUser.getEmail());
+        assertEquals(dbUser.getPassword(), savedUser.getPassword());
+        assertEquals(dbUser.getCreatedAt(), savedUser.getCreatedAt());
+        assertEquals(dbUser.getUpdatedAt(), savedUser.getUpdatedAt());
+    }
 }
