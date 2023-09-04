7ba9a4ceb76a534bbfdd2cf8d6678481e28c7e4d
Kunho Lee <gunho1020@gmail.com>
2023-09-04 11:32:21 +0900

    상품 엔티티의 생성일, 수정일 자동으로 관리하기 (#25)
    * feat: @EnableJpaAuditing 추가
    
    - JPA 가 이벤트, 생명 주기 훅에 따라 자동으로 생성, 수정 이력을 검사해주는 기능
    
    * feat: Item 엔티티에 생성일, 수정일 자동 검사 기능 추가
    
    - JPA 의 AuditingEntityListener 이벤트 리스너 추가
    - @CreatedAt 로 생성일 자동 추적
    - @LastModifiedDate 로 수정일 자동 추적
    - createdAt 은 수정 불가 column 으로 지정
    
    * feat: Item 엔티티에 builder 추가
    
    * feat: awaitility 라이브러리 추가
    
    - 비동기 테스트 작성을 도와주는 라이브러리
    - 수정일 자동 업데이트를
    
    * test: 자동 생성일, 수정일 관리 테스트 작성
    
    - 자동으로 생성일 등록
    - 자동으로 수정일 업데이트
    
    * feat: Item 엔티티의 생성일, 수정일 null 허용 안 함
    
    - 자동 관리되기 때문에 null 들어갈 일이 없음
    
    * refactor: Item 엔티티 생성에 builder 사용
    
    * fix: DataJapTest 에 AutoConfigureTestDatabase 추가
    
    - USER 예약에 때문에 추가함
    - 나중에 datasource.url 을 실제 DB 로 바꿀 때 test profile 에서 h2 db url 설정 지금처럼 유지해야 할 듯
    - 예약어 문제 다른 방법으로 풀면 제거 가능

--- src/main/java/hello/until/item/entity/Item.java
+++ src/main/java/hello/until/item/entity/Item.java
@@ -1,23 +1,30 @@
 package hello.until.item.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class Item {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private Integer price;
+    @Column(updatable = false, nullable = false)
+    @CreatedDate
     private LocalDateTime createdAt;
+    @Column(nullable = false)
+    @LastModifiedDate
     private LocalDateTime updatedAt;
 }
