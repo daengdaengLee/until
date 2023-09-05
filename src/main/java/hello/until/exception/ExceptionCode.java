0ff0048b0f8bab7854d2ffc346e42f31d6f223c6
daengdaengLee <gunho1020@gmail.com>
2023-09-05 17:57:34 +0900

    feat: ExceptionCode 의 JSON 직렬화 설정 추가
    - enum 을 JSON Object 형태로 직렬화
    - httpStatus 항목은 HTTP 헤더로 전달하기 때문에 직렬화 과정에서 제외

--- src/main/java/hello/until/exception/ExceptionCode.java
+++ src/main/java/hello/until/exception/ExceptionCode.java
@@ -1,14 +1,18 @@
 package hello.until.exception;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 @Getter
 @AllArgsConstructor
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum ExceptionCode {
     NO_ITEM_TO_UPDATE(HttpStatus.BAD_REQUEST, "수정할 상품이 없습니다.");
 
+    @JsonIgnore
     private final HttpStatus httpStatus;
     private final String message;
 }
