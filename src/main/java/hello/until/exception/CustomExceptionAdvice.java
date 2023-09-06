d35c6ba132da38aa39b857606dad47198b118b26
박상민 <87235273+tableMinPark@users.noreply.github.com>
2023-09-06 23:42:42 +0900

    상품 다중 조회 API 앤드포인트 개발 (#53)
    * feat : 상품 다중 조회 API 앤드포인트 개발
    
    - 페이지, 사이즈로 다중 상품 조회
    - 페이지 0 미만, 사이즈 1 미만인 경우에 400 으로 응답
    - 등록된 상품 목록 반환 dto ReadAllItemResponse 생성
    - Validated 를 통해 page, size 값 검증
    
    * feat : ItemController#readAllItem 메소드 명 수정
    
    - readAllItem 을 readAllItems 로 수정
    
    * feat : ItemController#readAllItems @Validated 예외 발생 처리
    
    - @RequestParam 의 @Validated 유효성 검사에서 실패하면 ConstraintViolationException 이 발생하는데, @ReuqestBody 처럼 ExceptionHandler 가 없어 예외가 발생하고 500 응답
    - ConstraintViolationException 을 Advice 로 처리해서 400 으로 응답
    
    * test : readAllItems 테스트 작성
    
    - 페이와 사이즈로 상품 목록을 조회하고 상품 리스트로 응답
    - 페이지가 없을 시 400 응답
    - 사이즈가 없을 시 400 응답
    - 페이지와 사이즈 없이 조회하면 400 응답
    - 페이지가 0 미만으로 조회하면 400과 실패 메세지 응답
    - 사이즈가 1 미만으로 조회하면 400과 실패 메시지 응답
    
    * feat : ItemController#readAllItems 수정
    
    - page, size의 값이 입력되지 않아도 기본값으로 조회가 가능하도록 수정
    - page defaultValue 0 으로 지정
    - size defaultValue 10 으로 지정
    
    * test : ItemControllerTest.ReadAll#readAllItemNoPageAndSize 수정
    
    - page, size 값이 입력되지 않았을 때 page의 기본값 0, size의 기본값 10 으로 목록을 조회하는 테스트로 수정

--- src/main/java/hello/until/exception/CustomExceptionAdvice.java
+++ src/main/java/hello/until/exception/CustomExceptionAdvice.java
@@ -1,11 +1,18 @@
 package hello.until.exception;
 
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class CustomExceptionAdvice {
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<?> validatedExceptionHandler(ConstraintViolationException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<ExceptionCode> exceptionHandler(CustomException e){
         return new ResponseEntity<>(e.getCode(), e.getCode().getHttpStatus());
--- src/main/java/hello/until/item/controller/ItemController.java
+++ src/main/java/hello/until/item/controller/ItemController.java
@@ -3,13 +3,18 @@ package hello.until.item.controller;
 import hello.until.item.dto.request.CreateItemRequest;
 import hello.until.item.dto.request.UpdateItemRequest;
 import hello.until.item.dto.response.ItemResponse;
+import hello.until.item.dto.response.CreateItemResponse;
+import hello.until.item.dto.response.ReadAllItemResponse;
+import hello.until.item.dto.response.ReadItemResponse;
 import hello.until.item.service.ItemService;
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+@Validated
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/items")
@@ -21,6 +26,12 @@ public class ItemController {
         return this.itemService.readItem(id)
                 .map(ItemResponse::new)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @GetMapping
+    public ReadAllItemResponse readAllItems(@RequestParam(required = false, defaultValue = "0") @Min(value = 0, message = "페이지는 0 이상이여야 합니다.") Integer page,
+                                           @RequestParam(required = false, defaultValue = "10") @Min(value = 1, message = "사이즈는 1 이상이여야 합니다.") Integer size) {
+        return new ReadAllItemResponse(this.itemService.readAllItems(page, size));
     }
 
     @PostMapping
