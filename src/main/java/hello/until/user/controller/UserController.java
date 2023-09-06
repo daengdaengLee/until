5dbe7a95a72cfc8c9782b2ca4721029eb430f6c0
jinseong <jinseong205@gmail.com>
2023-09-06 17:00:21 +0900

    feat : User role 추가
    import, 중복 된 불필요 library 제거

--- src/main/java/hello/until/user/controller/UserController.java
+++ src/main/java/hello/until/user/controller/UserController.java
@@ -1,28 +1,25 @@
 package hello.until.user.controller;
 
-import hello.until.user.dto.request.UpdateUserRequest;
-import hello.until.user.dto.response.UserResponse;
-import hello.until.user.entity.User;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 import hello.until.exception.CustomException;
 import hello.until.exception.ExceptionCode;
 import hello.until.user.dto.request.CreateUserRequest;
+import hello.until.user.dto.request.UpdateUserRequest;
 import hello.until.user.dto.response.GetUserResponse;
+import hello.until.user.dto.response.UserResponse;
+import hello.until.user.entity.User;
 import hello.until.user.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
