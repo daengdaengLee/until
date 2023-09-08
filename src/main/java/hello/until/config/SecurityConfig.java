667ca62edbb055720ca279f8fafb78e951521936
jinseong <jinseong205@gmail.com>
2023-09-08 11:23:20 +0900

    - 유저 패스워드 암호화
    기존 BCryptPasswordEncode 반환 -> PasswordEncode Interface로 반환

--- src/main/java/hello/until/config/SecurityConfig.java
+++ src/main/java/hello/until/config/SecurityConfig.java
@@ -3,12 +3,13 @@ package hello.until.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+    public PasswordEncoder  bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
